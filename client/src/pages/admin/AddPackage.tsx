46bcb47dbc85b40aab95465414fba1aca5e057fe
NABE Bammoy Minkitam <nabe.minkitam@gmail.com>
2024-03-04 11:16:33 +0000

    Update AddPackage.tsx

--- client/src/pages/admin/AddPackage.tsx
+++ client/src/pages/admin/AddPackage.tsx
@@ -4,8 +4,8 @@ import { useNavigate, useParams } from 'react-router-dom';
 import usePackage from '../../hooks/usePackage';
 import { ERROR_MESSAGE, SUCCESS_MESSAGE } from '../../utils/constant';
 import { toast } from 'react-toastify'
-import GooglePlaceInput from '../../components//GooglePlaceInput';
-import { loadScript } from '../../components//GooglePlaceInput';
+import GooglePlaceInput from '../../components/GooglePlaceInput';
+import { loadScript } from '../../components/GooglePlaceInput';
 
 function AddPackage() {
     const [loading, setLoading] = useState<boolean>(false)
